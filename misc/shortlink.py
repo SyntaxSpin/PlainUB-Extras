45eaf4b798b4c1d71ead93f4b1ba73a7b65684ba
R0X <patryk.bieganski1003@gmail.com>
2025-07-28 01:05:41 +0200

    Update shortlink.py

--- misc/shortlink.py
+++ misc/shortlink.py
@@ -36,7 +36,7 @@ async def shortlink_handler(bot: BOT, message: Message):
         await message.delete()
         return
 
-    progress_message = await message.reply("<i>Shortening link...</i>")
+    progress_message = await message.reply("Shortening link...")
     
     try:
         shortened_url = await asyncio.to_thread(sync_shorten, url_to_shorten)
