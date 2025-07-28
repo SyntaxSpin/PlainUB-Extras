302dba4b2e4e6ff5f35910d29298cc729db73942
R0X <patryk.bieganski1003@gmail.com>
2025-07-28 14:38:24 +0200

    Create kernelsu_next.py

---
+++ android/kernelsu_next.py
@@ -0,0 +1,7 @@
+from app import BOT, bot, Message
+from .utils import get_android_versions
+
+@bot.add_cmd(cmd="kernelsunext")
+async def kernelsunext_handler(bot: BOT, message: Message):
+    """CMD: KERNELSUNEXT - Gets the latest KernelSU-Next release."""
+    await get_android_versions(bot, message, owner="KernelSU-Next", repo="KernelSU", show_both=False)
