7433b5f146812943bdffc69a6640b527d54b0a52
R0X <patryk.bieganski1003@gmail.com>
2025-07-28 14:38:50 +0200

    Update kernelsu_next.py

--- android/kernelsu_next.py
+++ android/kernelsu_next.py
@@ -4,4 +4,4 @@ from .utils import get_android_versions
 @bot.add_cmd(cmd="kernelsunext")
 async def kernelsunext_handler(bot: BOT, message: Message):
     """CMD: KERNELSUNEXT - Gets the latest KernelSU-Next release."""
-    await get_android_versions(bot, message, owner="KernelSU-Next", repo="KernelSU", show_both=False)
+    await get_android_versions(bot, message, owner="KernelSU-Next", repo="KernelSU-Next", show_both=False)
