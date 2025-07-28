ed24cd3ab08e027dcea6fe4d4aa6088987cd2109
R0X <patryk.bieganski1003@gmail.com>
2025-07-28 18:37:02 +0200

    Update tts.py

--- fun/tts.py
+++ fun/tts.py
@@ -49,7 +49,7 @@ async def tts_handler(bot: BOT, message: Message):
         await message.edit("The message contains no text to convert.", del_in=ERROR_VISIBLE_DURATION)
         return
 
-    progress_message = await message.reply("Converting text to speech... 🎤")
+    progress_message = await message.reply("Converting text to speech...")
     
     file_path = ""
     try:
