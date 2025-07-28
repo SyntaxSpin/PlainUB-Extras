d3ecba14837a92ff976c4b5bb074eff6e57c54f7
R0X <patryk.bieganski1003@gmail.com>
2025-07-28 16:45:42 +0200

    Update ascii.py

--- fun/ascii.py
+++ fun/ascii.py
@@ -1,8 +1,11 @@
 import html
 import pyfiglet
+import asyncio
 from pyrogram.types import Message
 
 from app import BOT, bot
+
+ERROR_VISIBLE_DURATION = 8
 
 @bot.add_cmd(cmd="ascii")
 async def ascii_handler(bot: BOT, message: Message):
@@ -14,21 +17,18 @@ async def ascii_handler(bot: BOT, message: Message):
     
     text_to_convert = message.input
     if not text_to_convert:
-        await message.edit("Please provide text to convert. Usage: `.ascii Hello`")
+        await message.edit("Please provide text to convert. Usage: `.ascii Hello`", del_in=ERROR_VISIBLE_DURATION)
         return
 
-    # Generate the ASCII art
     try:
         ascii_text = pyfiglet.figlet_format(text_to_convert)
-        
-        # Format for Telegram's monospace font
         final_text = f"<pre language=ascii>{html.escape(ascii_text)}</pre>"
         
-        # Check if the message is too long for Telegram
         if len(final_text) > 4096:
-            await message.edit("The resulting ASCII art is too long to be sent.")
-        else:
-            await message.edit(final_text)
+            await message.edit("The resulting ASCII art is too long to be sent.", del_in=ERROR_VISIBLE_DURATION)
+            return
+
+        await message.edit(final_text)
 
     except Exception as e:
-        await message.edit(f"<b>Error:</b> Could not generate ASCII art.\n<code>{html.escape(str(e))}</code>")
+        await message.edit(f"<b>Error:</b> Could not generate ASCII art.\n<code>{html.escape(str(e))}</code>", del_in=ERROR_VISIBLE_DURATION)
