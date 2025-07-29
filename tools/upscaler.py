ff59c351b24dda8d1cb76780b2231531461a1525
SyntaxSpin <SyntaxSpin@users.noreply.github.com>
2025-07-29 15:52:20 +0200

    Update upscaler.py

--- tools/upscaler.py
+++ tools/upscaler.py
@@ -43,10 +43,11 @@ async def upscale_handler(bot: BOT, message: Message):
         .upscale
     """
     replied_msg = message.replied
-    if not replied_msg or not replied_msg.photo:
-        await message.edit("Please reply to an image to upscale it.", del_in=ERROR_VISIBLE_DURATION)
+    is_photo = replied_msg and (replied_msg.photo or (replied_msg.document and replied_msg.document.mime_type.startswith("image/")))
+    if not is_photo:
+        await message.edit("Please reply to an image to use this command.", del_in=ERROR_VISIBLE_DURATION)
         return
-
+    
     progress_message = await message.reply("<code>Downloading photo...</code>")
     
     original_path, upscaled_path = "", ""
