64cb7425cb29de4b8fffcdc2d3c16b7c0ef12d5c
SyntaxSpin <SyntaxSpin@users.noreply.github.com>
2025-08-01 15:28:34 +0200

    Add files via upload

---
+++ fun/cowsay.py
@@ -0,0 +1,15 @@
+from cowpy import cow
+
+from app import BOT, Message, bot
+
+
+@bot.add_cmd(cmd="cowsay")
+async def cowsay(bot: BOT, message: Message):
+    text = message.input
+    if not text:
+        await message.reply("What is the cow supposed to say?")
+        return
+
+    cow_said = cow.Cowacter().milk(text)
+
+    await message.reply(f"```\n{cow_said}\n```")
