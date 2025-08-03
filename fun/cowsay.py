afd3d8456f22c71d2a171ac631754039cdaea422
SyntaxSpin <SyntaxSpin@users.noreply.github.com>
2025-08-03 17:52:01 +0200

    Update cowsay.py

--- fun/cowsay.py
+++ fun/cowsay.py
@@ -17,7 +17,7 @@ async def cowsay(bot: BOT, message: Message):
     """
     text = message.input
     if not text:
-        await message.reply("What is the cow supposed to say?")
+        await message.reply("What is the cow supposed to say?", del_in=8)
         return
 
     string_io = io.StringIO()
