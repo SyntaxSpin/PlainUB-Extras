6a23d10630445b40c074e84270d10a5b02f0dcea
R0X <patryk.bieganski1003@gmail.com>
2025-08-03 16:30:21 +0200

    Update makeqr.py

--- misc/makeqr.py
+++ misc/makeqr.py
@@ -50,7 +50,7 @@ async def make_qr_handler(bot: BOT, message: Message):
             chat_id=message.chat.id,
             photo=output_path,
             caption=text_caption,
-            reply_params = ReplyParameters(message_id=message.replied.id)
+            reply_parameters = ReplyParameters(message_id=message.replied.id)
         )
         
         await progress_msg.delete()
