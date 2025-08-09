e06efaa4e9d5f7f91ae6faab43f0ecf6857b2606
R0X <patryk.bieganski1003@gmail.com>
2025-08-09 22:31:22 +0200

    Create sban.py

---
+++ admin/sban.py
@@ -0,0 +1,24 @@
+from pyrogram.types import User
+
+from app import BOT, Message
+
+@bot.add_cmd(cmd="sban")
+async def silent_ban_handler(bot: BOT, message: Message) -> None:
+    """
+    CMD: SBAN
+    INFO: Silently bans a user without sending a confirmation message.
+    USAGE:
+        .sban [user_id/@username/reply]
+    """
+    if not message.chat._raw.admin_rights:
+        return
+
+    user, _ = await message.extract_user_n_reason()
+
+    if not isinstance(user, User):
+        return
+
+    try:
+        await bot.ban_chat_member(chat_id=message.chat.id, user_id=user.id)
+    except Exception:
+        pass
