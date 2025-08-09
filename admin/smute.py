ef6b958f4f71110e2eb3af7374f0591915610bbc
R0X <patryk.bieganski1003@gmail.com>
2025-08-09 22:32:04 +0200

    Create smute.py

---
+++ admin/smute.py
@@ -0,0 +1,28 @@
+from pyrogram.types import ChatPermissions, User
+
+from app import BOT, Message
+
+@bot.add_cmd(cmd="smute")
+async def silent_mute_handler(bot: BOT, message: Message):
+    """
+    CMD: SMUTE
+    INFO: Silently mutes a user without sending a confirmation message.
+    USAGE:
+        .smute [user_id/@username/reply]
+    """
+    if not message.chat._raw.admin_rights:
+        return
+
+    user, _ = await message.extract_user_n_reason()
+
+    if not isinstance(user, User):
+        return
+
+    try:
+        await bot.restrict_chat_member(
+            chat_id=message.chat.id,
+            user_id=user.id,
+            permissions=ChatPermissions(can_send_messages=False)
+        )
+    except Exception:
+        pass
