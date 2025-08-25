8cb64e604b2b1ff9e1770fac6af4f18cc8af4054
SyntaxSpin <SyntaxSpin@users.noreply.github.com>
2025-08-25 15:11:32 +0200

    Update fetchcreator.py

--- tg_tools/fetchcreator.py
+++ tg_tools/fetchcreator.py
@@ -57,7 +57,7 @@ async def forward_info_handler(bot: BOT, message: Message):
             info_lines.append("• <b>Chat Link:</b> Not available (private)")
             
     else:
-        message.reply("Could not determine the original sender. Possible that the message creator has disabled profile linking for forwarded messages.")
+        info_lines.append("Could not determine the original sender. Possible that the message creator has disabled profile linking for forwarded messages.")
 
     await bot.send_message(
         chat_id=message.chat.id,
